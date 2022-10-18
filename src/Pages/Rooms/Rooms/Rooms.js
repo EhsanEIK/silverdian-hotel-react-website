0446c3a5ab1431a1b444e1fd542d1edcef1b2bfe
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-18 22:38:39 +0600

    added Home and Rooms component and routes

---
+++ src/Pages/Rooms/Rooms/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rooms = () => {
+    return (
+        <div>
+            <h1>Total Rooms: 56</h1>
+        </div>
+    );
+};
+
+export default Rooms;
--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
+import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import Rooms from "../../Pages/Rooms/Rooms/Rooms";
 import SignUp from "../../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
@@ -8,8 +10,11 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Main></Main>,
         children: [
-            { path: '/', element: <div>Home</div> },
-        ]
+            { path: '/', element: <Home></Home> },
+            { path: '/home', element: <Home></Home> },
+            { path: '/rooms', element: <Rooms></Rooms> },
+        ],
+        errorElement: <div>ERROR 404 FOUND!</div>
     },
     { path: '/login', element: <Login></Login> },
     { path: '/signup', element: <SignUp></SignUp> },
