5d3ddc2b2869c585f96aeb8add6e96e2d775a29c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-18 23:56:17 +0600

    added footer

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='mt-10 mb-2'>
+            <p className='text-center'>&copy; 2022 - Developed by <a href="https://www.linkedin.com/in/ehsanulislamkhan/" target="_blank" rel="noopener noreferrer" class="text-warning text-decoration-none">Ehsan</a></p>
+        </footer>
+    );
+};
+
+export default Footer;
