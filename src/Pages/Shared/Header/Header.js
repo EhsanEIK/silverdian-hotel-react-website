7c5599e6550ef721308d8d914eba51ce12c01687
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-18 21:45:25 +0600

    added navbar

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar></Navbar>
+        </header>
+    );
+};
+
+export default Header;
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Silverdian Hotel</Link>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal p-0">
+                    <li><Link to='/home'>Home</Link></li>
+                    <li><Link to='/rooms'>Rooms</Link></li>
+                    <li><Link to='/about'>About Us</Link></li>
+                    <li><Link to='/contact'>Contact Us</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
