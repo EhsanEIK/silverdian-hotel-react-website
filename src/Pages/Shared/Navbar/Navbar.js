6415d69d422a3269a87fe6ee56f974a1637e61a0
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-19 14:12:36 +0600

    added navbar baricon for mobile device

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,24 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/UserContext';
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
+    const [open, setOpen] = useState(false);
 
     const handleLogOut = () => {
         logOut().then(r => { }).catch(error => console.error(error));
     }
 
     return (
-        <div className="navbar bg-base-100">
+        <div className={`navbar bg-base-100 ${open ? 'pb-20' : 'pb-0'}`}>
             <div className='container mx-auto'>
                 <div className="flex-1">
                     <Link className="btn btn-ghost normal-case text-xl" to='/'>Silverdian Hotel</Link>
                 </div>
+                <div onClick={() => setOpen(!open)} className='cursor-pointer md:hidden block'>
+                    {
+                        open ? <FaTimes /> : <FaBars />
+                    }
+                </div>
                 {user?.email || user?.displayName ? < p className='text-blue-500 font-semibold mr-5'>Welcome, {user?.email || user?.displayName}</p>
                     : ''}
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal p-0">
+                <div className={`md:flex-none md:static absolute duration-500 ${open ? 'top-10' : '-top-96'}`}>
+                    <ul className="menu menu-horizontal flex-wrap md:w-auto w-96 p-0 m-0">
                         <li><Link to='/home'>Home</Link></li>
                         <li><Link to='/rooms'>Rooms</Link></li>
                         <li><Link to='/about'>About Us</Link></li>
