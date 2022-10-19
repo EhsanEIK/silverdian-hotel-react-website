e313a39d7767e7d8b5ad94c0dff4a7e2e5b0f164
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-19 08:25:27 +0600

    set provider value in UserContext file

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import app from '../firebase/firebase.config';
 import { getAuth } from 'firebase/auth';
 
@@ -7,8 +7,12 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
+    const [user, setUser] = useState('');
+
+    const authInfo = {};
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
